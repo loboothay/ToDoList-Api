efdda0aa37d18491494645c07769c9b4a78c0499
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-01-06 13:57:24 -0300

    Implementando a classe Modelo

---
+++ Api_ToDoMongo/Models/TarefaModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Api_ToDoMongo.Models
+{
+    public class TarefaModel
+    {
+        public TarefaModel(string nome, string descricao)
+        {
+            Id = Guid.NewGuid().ToString();
+            Nome = nome;
+            Descricao = descricao;
+            Status = false;
+            DataCadastro = DateTime.UtcNow;
+            DataConclusao = null;
+        }
+
+        public string Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Status { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public DateTime? DataConclusao { get; private set; }
+    }
+}
