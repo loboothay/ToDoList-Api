7bf3c65fb9e515cf4ff68fb6b61e821f3e1e7116
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-01-06 14:38:48 -0300

    Implementando injeção de dependencia

--- Api_ToDoMongo/Startup.cs
+++ Api_ToDoMongo/Startup.cs
@@ -1,3 +1,5 @@
+using Api_ToDoMongo.Data.Configurations;
+using Api_ToDoMongo.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,6 +29,9 @@ namespace Api_ToDoMongo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseConfiguration>(Configuration.GetSection(nameof(DatabaseConfiguration)));
+            services.AddScoped<IDatabaseConfiguration>(sp => sp.GetRequiredService<IOptions<DatabaseConfiguration>>().Value);
+            services.AddScoped<ITarefasRepository, TarefasRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
