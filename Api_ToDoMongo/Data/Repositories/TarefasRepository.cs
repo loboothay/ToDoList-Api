eceebca6cad8844b3cec6ebcbdb4381723b6da62
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-01-06 14:58:17 -0300

    Finalizando a implementação do repository

--- Api_ToDoMongo/Data/Repositories/TarefasRepository.cs
+++ Api_ToDoMongo/Data/Repositories/TarefasRepository.cs
@@ -1,28 +1,40 @@
-﻿using Api_ToDoMongo.Models;
+﻿using Api_ToDoMongo.Data.Configurations;
+using Api_ToDoMongo.Models;
+using MongoDB.Driver;
 using System.Collections.Generic;
 
 namespace Api_ToDoMongo.Data.Repositories
-{
+{    
     public class TarefasRepository : ITarefasRepository
     {
+        private readonly IMongoCollection<TarefaModel> _tarefas;
+
+        public TarefasRepository(IDatabaseConfiguration databaseConfiguration)
+        {
+            var client = new MongoClient(databaseConfiguration.ConnectionString);
+            var database = client.GetDatabase(databaseConfiguration.DatabaseName);
+
+            _tarefas = database.GetCollection<TarefaModel>("tarefas");
+        }
+
         public void Adicionar(TarefaModel tarefa)
         {
-            throw new System.NotImplementedException();
+            _tarefas.InsertOne(tarefa);
         }
 
         public void Atualizar(string id, TarefaModel tarefa)
         {
-            throw new System.NotImplementedException();
+            _tarefas.ReplaceOne(x => x.Id == id, tarefa);
         }
 
         public IEnumerable<TarefaModel> Buscar()
         {
-            throw new System.NotImplementedException();
+            return _tarefas.Find(tarefas => true).ToList();
         }
 
         public TarefaModel BuscarPorId(string id)
         {
-            throw new System.NotImplementedException();
+            return _tarefas.Find(tarefas => tarefas.Id == id).FirstOrDefault();
         }
     }
 }
