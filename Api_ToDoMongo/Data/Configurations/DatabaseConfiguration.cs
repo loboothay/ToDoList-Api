1c93d900a96846f1ec18a9ecfb36054958f96f11
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-01-06 14:19:48 -0300

    Implementando as classes de configuração para acesso aos dados

---
+++ Api_ToDoMongo/Data/Configurations/DatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Api_ToDoMongo.Data.Configurations
+{
+    public class DatabaseConfiguration : IDatabaseConfiguration
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Api_ToDoMongo/Data/Configurations/IDatabaseConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Api_ToDoMongo.Data.Configurations
+{
+    public interface IDatabaseConfiguration
+    {
+        string DatabaseName { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
